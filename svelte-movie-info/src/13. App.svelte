3ec13c0abb2d7d4d20427b197df50f0392184685
Jeon Hyejin <hyejda00@naver.com>
2026-03-02 16:10:47 +0900

    13. 하위 컴포넌트에 props 전달하기

---
+++ svelte-movie-info/src/13. App.svelte
@@ -0,0 +1,73 @@
+<script>
+import data  from './lib/movies'; 
+import Navbar from './lib/components/Navarbar.svelte';
+import Modal from './lib/components/Modal.svelte';
+
+  const handleLike = (i) => {
+    data[i].likeCount += 1;
+  }
+  
+  let isModal = false; 
+  let selectedMovie = 0;
+</script>
+
+<Navbar />
+<main class="container">
+  <h1>영화정보</h1>
+    {#each data as movie, i}
+    <div class="item">
+      <figure>
+        <img src={movie.imgUrl} alt={movie.title}>
+      </figure>
+      <div class="info">
+        <h3 class="bg-yellow">{movie.title}</h3>
+        <p>개봉: {movie.year}</p>
+        <p>장르: {movie.category}</p>
+        <button on:click={()=>{handleLike(i)}}>좋아요 {data[i].likeCount}</button>
+        <button 
+          on:click={()=>{
+            isModal = true; 
+            selectedMovie = i;
+            console.log('Selected movie: ' , data[i]);
+          }} 
+          class="btn btn-primary"
+        >상세보기</button>
+      </div>
+    </div>
+  {/each}
+</main>
+
+<!-- props : 요소에 속성을 정의하여 값을 전달화는 방식 
+      <요소명 props이름 = {전달값 | 변수명} -->
+{#if isModal}
+  <Modal data={data} selectedMovie={selectedMovie}/>
+{/if}
+
+<style>
+  .bg-yellow {
+    background: gold;
+    padding: 10px;
+    color: #333;
+  }
+
+  .item {
+    width: 100%;
+    border: 1px solid #ccc;
+    display: flex;
+    margin-bottom: 20px;
+    padding: 1rem;
+  }
+
+  .item figure {
+    width: 30%;
+    margin-right: 1rem;
+  }
+
+  .item img {
+    width: 100%;
+  }
+
+  .item .info {
+    width: 100%;
+  }
+</style>
---
+++ svelte-movie-info/src/lib/components/Modal.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let data; // props로 내린 영화 데이터를 받는다  
+  export let selectedMovie;
+</script>
+
+<div class="modal">
+  <div class="inner">
+    <h3>{data[selectedMovie].title}</h3>
+    <p>{@html data[selectedMovie].story}</p>
+    <button on:click={()=>{isModal=false;}} class="bth-close">닫기</button>
+  </div>  
+</div>
+
+<style>
+  
+  .modal {
+    background: rgba(0, 0, 0, 0.7);
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal .inner {
+    background: #fff;
+    width: 80%;
+    padding: 20px;
+    border-radius: 10px;
+  }
+</style>

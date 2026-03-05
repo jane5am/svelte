9700931996c0ac2f6b31ffe94fe52ce34c57ffa0
Jeon Hyejin <hyejda00@naver.com>
2026-03-05 10:37:13 +0900

    35. News 영역

--- bboy-home/src/App.svelte
+++ bboy-home/src/App.svelte
@@ -2,9 +2,11 @@
   import Router, {location, link} from 'svelte-spa-router';
   import Home from "./lib/components/Home.svelte";
   import About from "./lib/components/About.svelte";
+  import News from './lib/components/News.svelte';
 </script>
 
 <Router routes={{
   '/': Home,
-  '/about': About
+  '/about': About,
+  '/news': News
 }} />
---
+++ bboy-home/src/lib/components/News.svelte
@@ -0,0 +1,50 @@
+<script>
+  import Header from './Header.svelte';
+  import Footer from './Footer.svelte';
+  import news from '../../assets/news';
+</script>
+
+<Header />
+<main  class="container">
+  <h2>비보이 브라더스 활동</h2>
+  <div class="news">
+    <div class="news-item">
+      <img src={news[0].imgUrl} alt={news[0].title}>
+      <div class="item-text">
+        <h3>{news[0].title}</h3>
+        <p class="date">{news[0].date}</p>
+        <div class="hashtag">
+          <span>#{news[0].hashtag[0]}</span>
+          <span>#{news[0].hashtag[1]}</span>
+          <span>#{news[0].hashtag[2]}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+<Footer />
+
+<style lang="scss">
+  .news {
+    display: flex;
+    gap: 20px;
+    .item-text { padding: 20px; }
+    .news-item {
+      width: calc(100% / 3);
+      img {
+        width: 100%;
+      }
+      box-shadow: 4px 4px 10px rgba(0,0,0,0.2);
+    }
+    h3 { margin-bottom: 10px;}
+    p { margin-bottom: 4px;}
+    .date, .hashtag {
+      font-size: 15px;
+      color: #666;
+    }
+  }
+
+  .container {
+    background: #fff;
+  }
+</style>
